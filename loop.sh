625ac5ddbea8c1917381350dec2d1355fa374878
Jade <hmy940118@gmail.com>
2019-05-30 19:01:20 +0800

    Update 2019-05-30 19:01:20

---
+++ loop.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# @Description : GreenBox
+#
+# @Date        : 2019-06-12 11:15
+# @Author      : Jade
+#
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+export PATH
+
+if [ ! $1 ]; then  
+   echo 'ERROR: Need to apply loop param'    
+   exit  
+fi  
+for i in $(seq 1 $1)
+do 
+   time=`date +'%G-%m-%d %H:%M:%S' -d '-1 hours'`
+   timedatectl set-time "$time"
+   `sh ./init.sh`
+done
